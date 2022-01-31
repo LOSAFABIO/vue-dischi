3b23a601936d940e6cf31414ae319661334404f5
LOSAFABIO <fabio.losa95@gmail.com>
2022-01-31 16:28:00 +0100

    creati i file e collegati tra loro

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container-fluid">
+  </div>
+</template>
+
+<script>
+export default {
+  name : "Header"
+}
+</script>
+
+<style>
+
+.container-fluid{
+  height: 80px;
+  background-color: rgb(109, 109, 109);
+}
+
+</style>
